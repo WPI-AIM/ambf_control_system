9f5afcc15c27d5ff31560457fa26d65309964aba
nathaniel <nagoldfarb@wpi.edu>
2022-01-08 12:25:08 -0500

    working on some of the structure of dynamic testing

---
+++ rbdl_model_tests/include/rbdl_model_tests/DynamicTesting.h
@@ -0,0 +1,108 @@
+#include <string>
+#include <iostream>
+#include <stdio.h> 
+#include <rbdl/rbdl.h>
+
+
+#include <boost/numeric/odeint/stepper/runge_kutta_cash_karp54.hpp>
+#include <boost/numeric/odeint/stepper/controlled_runge_kutta.hpp>
+#include <boost/numeric/odeint/integrate/integrate_adaptive.hpp>
+#include <boost/numeric/odeint/stepper/generation/make_controlled.hpp>
+//using namespace std;
+using namespace boost::numeric::odeint;
+
+
+
+using namespace RigidBodyDynamics;
+using namespace RigidBodyDynamics::Math;
+
+
+
+//====================================================================
+// Boost stuff
+//====================================================================
+
+typedef std::vector< double > state_type;
+
+typedef runge_kutta_cash_karp54< state_type > error_stepper_type;
+typedef controlled_runge_kutta< error_stepper_type > controlled_stepper_type;
+
+
+class rbdlToBoost {
+
+    public:
+        rbdlToBoost(Model* model) : model(model) {
+            q = VectorNd::Zero(model->dof_count);
+            qd = VectorNd::Zero(model->dof_count);
+            qdd = VectorNd::Zero(model->dof_count);
+            tau = VectorNd::Zero(model->dof_count);
+
+        }
+
+        //3c. Boost uses this 'operator()' function to evaluate the state
+        //    derivative of the pendulum.
+        void operator() (const state_type &x, 
+                         state_type &dxdt, 
+                         const double t){
+
+            //3d. Here we split out q (generalized positions) and qd 
+            //    (generalized velocities) from the x (state vector)
+            //q
+            int j = 0;            
+            for(int i=0; i<model->dof_count; i++){                
+                q[i] = (double)x[j];
+                j++;
+            }
+
+            //qd
+            for(int i=0; i<model->dof_count; i++){                
+                qd[i] = (double)x[j];
+                j++;
+            }
+
+            //3e. Here we set the applied generalized forces to zero
+            for(int i=0; i<model->dof_count; i++){                
+                tau[i] = 0;
+            }
+
+            //3f. RBDL's ForwardDynamics function is used to evaluate
+            //    qdd (generalized accelerations)
+            ForwardDynamics (*model,q,qd,tau,qdd);
+
+            //3g. Here qd, and qdd are used to populate dxdt 
+            //(the state derivative)
+            j = 0;
+            for(int i = 0; i < model->dof_count; i++){
+                dxdt[j] = (double)qd[i];
+                j++;
+            }            
+            for(int i = 0; i < model->dof_count; i++){
+                dxdt[j] = (double)qdd[i];
+                j++;
+            }
+
+
+        }
+
+    private:
+        Model* model;
+        VectorNd q, qd, qdd, tau;
+};
+
+struct pushBackStateAndTime
+{
+    std::vector< state_type >& states;
+    std::vector< double >& times;
+
+    pushBackStateAndTime( std::vector< state_type > &states , 
+                              std::vector< double > &times )
+    : states( states ) , times( times ) { }
+
+    void operator()( const state_type &x , double t )
+    {
+        states.push_back( x );
+        times.push_back( t );
+    }
+};
+
+void f(const state_type &x, state_type &dxdt, const double t);
--- rbdl_model_tests/src/KukaHCDynamicsTests.cc
+++ rbdl_model_tests/src/KukaHCDynamicsTests.cc
@@ -1,12 +1,13 @@
-#include "rbdl_model_tests/Kuka.h"
+#include "rbdl_model_tests/KUKA.h"
 #include "rbdl/Logging.h"
 
 #include "rbdl/Model.h"
 #include "rbdl/Dynamics.h"
 
+#include "rbdl_model_tests/DynamicTesting.h"
 
 #ifndef USE_SLOW_SPATIAL_ALGEBRA
-TEST_CASE_METHOD(Kuka, __FILE__"_TestCalcDynamicPositionNeutral", "") 
+TEST_CASE_METHOD(KUKA, __FILE__"_TestCalcDynamicPositionNeutral", "") 
 {
     // We call ForwardDynamics() as it updates the spatial transformation
     // matrices
@@ -41,4 +42,86 @@ TEST_CASE_METHOD(Kuka, __FILE__"_TestCalcDynamicPositionNeutral", "")
     usleep(250000);
   }
 }
-#endif
+ #endif
+
+
+
+TEST_CASE_METHOD(KUKA, __FILE__"_TestCalcDynamicPosition", "") 
+{
+    // We call ForwardDynamics() as it updates the spatial transformation
+    // matrices
+  // Initialization of the input vectors
+  int dof = (size_t)rbdlModel->dof_count;
+  VectorNd Q      = VectorNd::Constant (dof, 0.);
+  VectorNd QDot   = VectorNd::Constant (dof, 0.);
+  VectorNd QDDot  = VectorNd::Constant (dof, 0.);
+  VectorNd Tau    = VectorNd::Constant (dof, 0.);
+  VectorNd TauInv = VectorNd::Constant (dof, 0.);
+
+  VectorNd q   = VectorNd::Zero (dof);
+  VectorNd qd  = VectorNd::Zero (dof);
+
+  //problem specific constants
+  int     nPts    = 100;
+  double  t0      = 0;
+  double  t1      = 3;
+
+
+  double t        = 0;             //time
+  double ts       = 0;            //scaled time
+  double tp = 0;
+  double dt   = (t1-t0)/((double)nPts);   
+
+  //Integration settings
+  double absTolVal   = 1e-10;
+  double relTolVal   = 1e-6;
+  double a_x = 1.0 , a_dxdt = 1.0;
+  rbdlToBoost rbdlBoostModel(rbdlModel);
+
+  // model setup 
+  state_type xState(dof*2);
+  int steps = 0;
+
+  for(unsigned int i=0; i < 2*dof; ++i)
+  {
+    q[i] = 0.0;
+    qd[i+dof] = 0.0;
+    xState[i] = q[i];
+    xState[i+dof] = q[i+dof];
+  }
+
+
+
+  controlled_stepper_type controlled_stepper(
+                                             default_error_checker< double , range_algebra , default_operations >
+                                             ( absTolVal , relTolVal , a_x , a_dxdt ) 
+                                             );
+
+        
+
+  for(int i = 0; i < nPts; i++)
+  {
+
+      t = t0 + dt*i;
+
+      //3h. Here we integrate forward in time between a series of nPts from
+      //    t0 to t1
+      integrate_adaptive( 
+          controlled_stepper ,
+          rbdlBoostModel , xState , tp , t , (t-tp)/10 );
+
+      tp = t;
+
+
+      for(unsigned int i=0; i < 2*dof; ++i)
+      {
+        q[i] = xState[i];
+        qd[i+dof] = xState[i+dof];
+      }
+
+
+     
+  }
+
+
+}
